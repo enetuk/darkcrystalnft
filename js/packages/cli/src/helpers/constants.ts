33d799a3104eb9cfc1c987821151b38e00413c7f
Netuk <blackcrystal2022@gmail.com>
2022-03-24 14:38:02 +0300

    change code

--- js/packages/cli/src/helpers/constants.ts
+++ js/packages/cli/src/helpers/constants.ts
@@ -12,11 +12,9 @@ export const MAX_URI_LENGTH = 200;
 export const MAX_SYMBOL_LENGTH = 10;
 export const MAX_CREATOR_LEN = 32 + 1 + 1;
 export const MAX_CREATOR_LIMIT = 5;
-/*
 export const ARWEAVE_PAYMENT_WALLET = new PublicKey(
   '6FKvsq4ydWFci6nGq9ckbjYMtnmaqAoatz5c9XWjiDuS',
 );
-
 export const CANDY_MACHINE_PROGRAM_ID = new PublicKey(
   'cndyAnrLdpjq1Ssp1z8xxDsB8dxe7u4HL5Nxi2K5WXZ',
 );
@@ -30,12 +28,10 @@ export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
 );
 export const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
   'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
-);*/
-
+);
 export const TOKEN_PROGRAM_ID = new PublicKey(
   'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
 );
-/*
 export const FAIR_LAUNCH_PROGRAM_ID = new PublicKey(
   'faircnAB9k59Y4TXmLabBULeuTLgV7TkGMGNkjnA15j',
 );
@@ -54,7 +50,7 @@ export const GUMDROP_DISTRIBUTOR_ID = new PublicKey(
 export const GUMDROP_TEMPORAL_SIGNER = new PublicKey(
   'MSv9H2sMceAzccBganUXwGq3GXgqYAstmZAbFDZYbAV',
 );
-*/
+
 export const CONFIG_ARRAY_START =
   32 + // authority
   4 +
